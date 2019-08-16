333d0ccb9f920adf94e2aab359f90c4eac349c39
Yang Liu <liuyang12@focusmedia.cn>
2019-08-16 11:02:32 +0800

    update

--- demo.py
+++ demo.py
@@ -1,21 +1,36 @@
 import os
+# import the necessary packages
+from imutils import paths
+import argparse
+import cv2
 
-from brisque import BRISQUE
+
+def variance_of_laplacian(image):
+    # compute the Laplacian of the image and then return the focus
+    # measure, which is simply the variance of the Laplacian
+    return cv2.Laplacian(image, cv2.CV_64F).var()
+
 
 if __name__ == "__main__":
-    brisque = BRISQUE()
+
     print('good:')
     good_folder = 'images/good'
     files = [file for file in os.listdir(good_folder) if file.endswith('.jpg')]
     for file in files:
+        print(file)
         path = os.path.join(good_folder, file)
-        score = brisque.get_score(path)
-        print(score)
+        image = cv2.imread(path)
+        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        fm = variance_of_laplacian(gray)
+        print(fm)
 
     print('bad:')
     bad_folder = 'images/bad'
     files = [file for file in os.listdir(bad_folder) if file.endswith('.jpg')]
     for file in files:
+        print(file)
         path = os.path.join(bad_folder, file)
-        score = brisque.get_score(path)
-        print(score)
+        image = cv2.imread(path)
+        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        fm = variance_of_laplacian(gray)
+        print(fm)
